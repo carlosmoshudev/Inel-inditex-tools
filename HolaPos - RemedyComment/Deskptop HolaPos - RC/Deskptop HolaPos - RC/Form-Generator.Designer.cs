7b151d88866af924963f691adea24b61bfaf57f3
carlosmoshudev <carlos@carlosmoshu.com>
2020-09-23 16:44:32 +0200

    Version de maquetado

--- HolaPos - RemedyComment/Deskptop HolaPos - RC/Deskptop HolaPos - RC/Form-Generator.Designer.cs
+++ HolaPos - RemedyComment/Deskptop HolaPos - RC/Deskptop HolaPos - RC/Form-Generator.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_Clipboard_gen = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 102);
+            this.textBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(473, 102);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(485, 129);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Comentario";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(485, 129);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pegar pruebas";
+            // 
+            // btn_Clipboard_gen
+            // 
+            this.btn_Clipboard_gen.Location = new System.Drawing.Point(18, 331);
+            this.btn_Clipboard_gen.Name = "btn_Clipboard_gen";
+            this.btn_Clipboard_gen.Size = new System.Drawing.Size(160, 33);
+            this.btn_Clipboard_gen.TabIndex = 4;
+            this.btn_Clipboard_gen.Text = "Clipboard";
+            this.btn_Clipboard_gen.UseVisualStyleBackColor = true;
+            this.btn_Clipboard_gen.Click += new System.EventHandler(this.btn_Clipboard_gen_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 282);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(485, 49);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "URL o nombre de procedimiento Confluence";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(473, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(511, 374);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btn_Clipboard_gen);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Generador de Comentario Remedy";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_Clipboard_gen;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HolaPos - RemedyComment/Deskptop HolaPos - RC/Deskptop HolaPos - RC/Form-Generator.cs
+++ HolaPos - RemedyComment/Deskptop HolaPos - RC/Deskptop HolaPos - RC/Form-Generator.cs
@@ -12,9 +12,21 @@ namespace Deskptop_HolaPos___RC
 {
     public partial class Form1 : Form
     {
+        private string _clipBoardSetterText;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_Clipboard_gen_Click(object sender, EventArgs e)
+        {
+            _clipBoardSetterText = "[SP] " + textBox1.Text + Environment.NewLine + Environment.NewLine;
+            _clipBoardSetterText += richTextBox1.Text.Trim() + Environment.NewLine + Environment.NewLine + Environment.NewLine;
+            if (textBox2.Text.Trim() != "")
+            {
+                _clipBoardSetterText += "En base a procedimiento: " + textBox2.Text.Trim();
+            }
+            Clipboard.SetText(_clipBoardSetterText);
         }
     }
 }
